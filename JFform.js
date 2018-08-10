5054fc3edd120214f965e234aaf6a7d1c855522e
Francisco J. Torres P <35893674+fjtorresperez@users.noreply.github.com>
2018-08-10 08:55:47 +0200

    update file

---
+++ JFform.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+
+
+if (typeof jQuery === 'undefined') {
+  throw new Error('JFform JavaScript requires jQuery')
+}
+
+(function($) {
+    "use strict";
+	$.fn.JFform = function( options ) {
+		alert('ok');
+	};
+
+}(jQuery));
